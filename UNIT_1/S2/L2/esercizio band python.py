9290c81dc4b4412316983296aa78086271792c2b
marcopator-beep <marcopator@gmail.com>
2026-06-16 15:32:04 +0200

    S2L2 esercizio Python

---
+++ UNIT_1/S2/L2/esercizio band python.py
@@ -0,0 +1,6 @@
+
+citta = input("In quale città sei cresciuto?\n")
+animale = input("Qual è il nome del tuo animale domestico?\n")
+
+nome_band = citta + " " + animale
+print("Il nome della tua band potrebbe essere: " + nome_band)
